f0cf1d891a041e1c790164f8bd27cb0f6bee1873
Lama <lama199703@gmail.com>
2021-07-15 04:34:41 +0300

    Adding contact form

--- config/mail.php
+++ config/mail.php
@@ -36,14 +36,15 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
+			'driver' => env('MAIL_DRIVER', 'smtp'),
             'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
             'from' => ['address' => 'lama.solutions.100@gmail.com', 'name' => 'LL'],
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'auth_mode' => null,
+            //'timeout' => null,
+            //'auth_mode' => null,
             'sendmail' => '/usr/sbin/sendmail -bs',
             'pretend' => false,
         ],
@@ -53,6 +54,11 @@ return [
 					'verify_peer' => false,
 					'verify_peer_name' => false,
 				],
+				'tls' => [
+					'allow_self_signed' => true,
+					'verify_peer' => false,
+					'verify_peer_name' => false,
+				],				
 		],		
 
         'ses' => [
